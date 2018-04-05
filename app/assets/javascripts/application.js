1e9eb73968f781d83ab7c785e098fa1bb6bf7a70
Ky Phong <nguyenvankyphong@gmail.com>
2018-04-05 18:55:52 +0700

    header+ sidebar + controller of categories

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,12 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+//= require vendor/BoxRefresh
+//= require vendor/BoxWidget
+//= require vendor/ControlSidebar
+//= require vendor/DirectChat
+//= require vendor/Layout
+//= require vendor/PushMenu
+//= require vendor/TodoList
+//= require vendor/Tree
---
+++ app/assets/javascripts/vendor/all.js
@@ -0,0 +1,8 @@
+//= require vendor/BoxRefresh
+//= require vendor/BoxWidget
+//= require vendor/ControlSidebar
+//= require vendor/DirectChat
+//= require vendor/Layout
+//= require vendor/PushMenu
+//= require vendor/TodoList
+//= require vendor/Tree
